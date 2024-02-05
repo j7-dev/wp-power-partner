81b236c7f9be212e8a7d84986bbc20ef2ae00b82
J7 <j7.dev.gg@outlook.com>
2024-02-06 07:30:45 +0800

    feat: 優化 SiteSelector 組件

--- components/SiteSelector/index.php
+++ components/SiteSelector/index.php
@@ -39,22 +39,20 @@ class SiteSelector extends Utils
 			if ((!\is_wp_error($response)) && (200 === \wp_remote_retrieve_response_code($response))) {
 				$responseBody = json_decode($response['body']);
 				if (json_last_error() === JSON_ERROR_NONE) {
-					\set_transient(self::TRANSIENT_KEY, $responseBody, 12 * HOUR_IN_SECONDS);
+					\set_transient(self::TRANSIENT_KEY, $responseBody, self::CACHE_TIME);
 					return $responseBody;
 				}
 			} else {
 				$responseBody = json_decode($response['body']);
 				ob_start();
 				print_r($responseBody);
-				$error = ob_get_clean();
-				error_log('⭐⭐ wp_remote_get [cloud]/wp-json/wpcd/v1/sites Error' . '[' . \wp_remote_retrieve_response_code($response) . '] ' . $error);
+				\J7\WpToolkit\Utils::debug_log('wp_remote_get [cloud]/wp-json/wpcd/v1/sites Error ' . ob_get_clean());
 				return [];
 			}
-		} catch (\Exception $ex) {
+		} catch (\Exception $error) {
 			ob_start();
-			print_r($ex);
-			$error = ob_get_clean();
-			error_log('⭐⭐ wp_remote_get /wp-json/wpcd/v1/sites Error ' . $error);
+			print_r($error);
+			\J7\WpToolkit\Utils::debug_log('wp_remote_get /wp-json/wpcd/v1/sites Error ' . ob_get_clean());
 			return [];
 		}
 	}
@@ -73,8 +71,7 @@ class SiteSelector extends Utils
 	{
 		$sites         = $this->getAllSites();
 		$templateSites = array_filter($sites, function ($site) {
-			// 根据属性进行过滤条件，这里假设你想要保留 propertyName 为 'value1' 的对象
-			return $site->server_id === self::TEMPLATE_SERVER_ID;
+			return in_array($site->server_id, self::TEMPLATE_SERVER_IDS);
 		});
 
 		return $templateSites;
@@ -98,17 +95,15 @@ class SiteSelector extends Utils
 		?>
 			<p>目前連結的網站: <a href="https://<?= $linked_site_obj->domain; ?>" target="_blank"><?= $linked_site_obj->name; ?></a> <span class="dashicons dashicons-wordpress"></span> <?= $linked_site_obj->wp_version; ?></p>
 		<?php endif; ?>
-		<select name="linked_site" id="linked_site">
+		<select id="linked_site">
 			<option value="">請選擇要連結的網站</option>
-			<?php foreach ($templateSites as $site) :
-				$selected = $site->id === (int) $defaultValue ? 'selected' : '';
-			?>
-				<option value="<?php echo $site->id; ?>" <?= $selected ?>><?php echo $site->name; ?></option>
+			<?php foreach ($templateSites as $site) : ?>
+				<option value="<?php echo $site->id; ?>" <?= selected($defaultValue, $site->id) ?>><?php echo $site->name; ?></option>
 			<?php endforeach; ?>
 		</select>
 
 		<p>或直接輸入網站 id</p>
-		<input name="linked_site" id="linked_site" />
+		<input name="linked_site" value="<?= $defaultValue ?>" />
 <?php
 		// TODO select / input 挑一個就好
 		$html = ob_get_clean();
