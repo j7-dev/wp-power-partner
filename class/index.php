c554425cde62c33b358e10648499fbddaf15c752
J7 <j7.dev.gg@outlook.com>
2024-02-06 07:30:11 +0800

    feat: 與 可變訂閱商品結合

--- class/index.php
+++ class/index.php
@@ -7,10 +7,6 @@ namespace J7\PowerPartner;
 use J7\PowerPartner\Components\SiteSelector;
 use J7\PowerPartner\SiteSync;
 
-require_once __DIR__ . '/class-site-sync.php';
-require_once __DIR__ . '/class-order-view.php';
-require_once __DIR__ . '/class-menu-page.php';
-
 /**
  * 新增 product type: power_partner
  */
@@ -18,54 +14,74 @@ class Bootstrap
 {
 	const PRODUCT_TYPE_SLUG = Utils::SNAKE;
 	const PRODUCT_TYPE_NAME      = Utils::APP_NAME . ' 產品';
+
 	public function __construct()
 	{
-		\add_action('add_meta_boxes', [$this, 'add_metabox']);
-		\add_filter('product_type_selector', array($this, 'add_product_type_to_dropdown'));
-		\add_action("save_post", [$this, "save_metabox"]);
+		require_once __DIR__ . '/class-site-sync.php';
+		require_once __DIR__ . '/class-order-view.php';
+		require_once __DIR__ . '/class-menu-page.php';
+
+		\add_action('admin_enqueue_scripts', array($this, 'enqueue_assets'));
+		\add_filter('woocommerce_product_data_tabs', array($this, 'add_product_tab'), 50, 1);
+		\add_action('woocommerce_product_data_panels', array($this, 'add_product_tab_content'));
+		\add_action('woocommerce_process_product_meta', array($this, 'save_product_tab_content'));
+
 		\add_action('woocommerce_order_status_completed', [$this, 'do_site_sync']);
 	}
 
-	public function add_metabox(): void
+	public function enqueue_assets(): void
 	{
-		\add_meta_box(Utils::SNAKE . '_metabox', '選擇要連結的網站', [$this, Utils::SNAKE . '_callback'], 'product');
-	}
-
-	public function add_product_type_to_dropdown(array $types): array
-	{
-		$types[self::PRODUCT_TYPE_SLUG] = __(self::PRODUCT_TYPE_NAME, Utils::SNAKE);
-
-		return $types;
-	}
-
-	public function power_partner_callback(): void
-	{
-		$post_id = $_GET['post'];
-		if (empty($post_id)) {
+		$screen = \get_current_screen();
+		if ($screen->id !== 'product') {
 			return;
 		}
+		\wp_enqueue_style(Utils::KEBAB . '-css', Utils::get_plugin_url() . '/js/dist/assets/css/index.css', [], Utils::get_plugin_ver());
+		\wp_enqueue_script(Utils::KEBAB . '-js', Utils::get_plugin_url() . '/js/dist/index.js', [], Utils::get_plugin_ver(), [
+			'strategy' => 'async',
+			'in_footer' => true,
+		]);
+	}
 
+	public function add_product_tab(array $tabs): array
+	{
+		$tabs[self::PRODUCT_TYPE_SLUG] = array(
+			'label'    => __('Power Partner', Utils::SNAKE),
+			'target'   => self::PRODUCT_TYPE_SLUG,
+			'class'    => [
+				'show_if_variable-subscription', // 僅在可變訂閱顯示
+			],
+			'priority' => 80,
+		);
+
+
+		return $tabs;
+	}
+
+	public function add_product_tab_content(): void
+	{
+		$post_id = $_GET['post'] ?? null;
 		$siteSelector = SiteSelector::getInstance();
-		$defaultValue = \get_post_meta($post_id, "linked_site", true) ?? null;
-		echo $siteSelector->render($defaultValue);
+		$defaultValue = \get_post_meta($post_id, "linked_site", true);
+?>
+		<div id="<?= self::PRODUCT_TYPE_SLUG ?>_product_data" style="float:left; width:80%;display:none;">
+			<div style="padding:1.5rem 1rem;">
+				<?= $siteSelector->render($defaultValue); ?>
+			</div>
+		</div>
+<?php
 	}
 
-	public function save_metabox($post_id): void
+	public function save_product_tab_content($post_id): void
 	{
-		$post_type = \get_post_type($post_id);
-
-		if ('product' !== $post_type) {
-			return;
+		if (isset($_POST['linked_site'])) {
+			\update_post_meta($post_id, "linked_site", $_POST['linked_site']);
 		}
-
-		$linked_site = $_POST['linked_site'] ?? null;
-		\update_post_meta($post_id, "linked_site", $linked_site);
 	}
 
 	public function do_site_sync($order_id): void
 	{
 
-		$order     = wc_get_order($order_id);
+		$order     = \wc_get_order($order_id);
 		$items     = $order->get_items();
 		$responses = [];
 		foreach ($items as $item) {
