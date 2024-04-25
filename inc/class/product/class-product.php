76eec73391bc2b1ff11135748d3144d4d9b2432b
J7 <j7.dev.gg@outlook.com>
2024-04-25 22:49:16 +0800

    fix: 試用期結束再開站的問題 #6

--- inc/class/product/class-product.php
+++ inc/class/product/class-product.php
@@ -28,7 +28,7 @@ final class Product {
 	 */
 	public function __construct() {
 		\add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_assets' ) );
-		\add_action( 'woocommerce_order_status_completed', array( $this, 'site_sync_by_order_id' ) );
+		\add_action( 'woocommerce_checkout_subscription_created', array( $this, 'site_sync_by_order_id' ), 20, 3 );
 	}
 
 	/**
@@ -58,15 +58,18 @@ final class Product {
 	/**
 	 * Do site sync
 	 *
-	 * @param int $order_id order id
+	 * @param \WC_Subscription $new_subscription The subscription object.
+	 * @param \WC_Order        $order                   The order object.
+	 * @param mix              $recurring_cart  The recurring cart.
 	 * @return void
 	 */
-	public function site_sync_by_order_id( $order_id ): void {
-		$order = \wc_get_order( $order_id );
+	public function site_sync_by_order_id($new_subscription, $order, $recurring_cart ): void { // phpcs:ignore
+
 		if ( ! $order ) {
 			return;
 		}
-		$items     = $order->get_items();
+
+		$items     = $order?->get_items();
 		$responses = array();
 
 		// 考慮訂單中每個商品都可能會綁定到不同的網站，所以做成 array
@@ -77,16 +80,16 @@ final class Product {
 			 *
 			 * @var \WC_Order_Item_Product $item
 			 */
-			$product_id = $item->get_product_id();
+			$product_id = $item?->get_product_id();
 			$product    = \wc_get_product( $product_id );
 
 			// 如果不是可變訂閱商品，就不處理
-			if ( 'variable-subscription' === $product->get_type() ) {
-				$variation_id      = $item->get_variation_id();
+			if ( 'variable-subscription' === $product?->get_type() ) {
+				$variation_id      = $item?->get_variation_id();
 				$host_position     = \get_post_meta( $variation_id, DataTabs::HOST_POSITION_FIELD_NAME, true );
 				$linked_site_id    = \get_post_meta( $variation_id, DataTabs::LINKED_SITE_FIELD_NAME, true );
 				$linked_site_ids[] = $linked_site_id;
-			} elseif ( 'subscription' === $product->get_type() ) {
+			} elseif ( 'subscription' === $product?->get_type() ) {
 				$host_position     = \get_post_meta( $product_id, DataTabs::HOST_POSITION_FIELD_NAME, true );
 				$linked_site_id    = \get_post_meta( $product_id, DataTabs::LINKED_SITE_FIELD_NAME, true );
 				$linked_site_ids[] = $linked_site_id;
@@ -107,20 +110,20 @@ final class Product {
 					'host_position' => $host_position,
 					'partner_id'    => \get_option( Plugin::SNAKE . '_partner_id', '0' ),
 					'customer'      => array(
-						'id'         => $order->get_customer_id(),
-						'first_name' => $order->get_billing_first_name(),
-						'last_name'  => $order->get_billing_last_name(),
-						'username'   => \get_user_by( 'id', $order->get_customer_id() )->user_login ?? 'admin',
-						'email'      => $order->get_billing_email(),
-						'phone'      => $order->get_billing_phone(),
+						'id'         => $order?->get_customer_id(),
+						'first_name' => $order?->get_billing_first_name(),
+						'last_name'  => $order?->get_billing_last_name(),
+						'username'   => \get_user_by( 'id', $order?->get_customer_id() )?->user_login ?? 'admin',
+						'email'      => $order?->get_billing_email(),
+						'phone'      => $order?->get_billing_phone(),
 					),
 				)
 			);
 
 			$responses[] = array(
-				'status'  => $response_obj->status,
-				'message' => $response_obj->message,
-				'data'    => $response_obj->data,
+				'status'  => $response_obj?->status,
+				'message' => $response_obj?->message,
+				'data'    => $response_obj?->data,
 			);
 		}
 		ob_start();
@@ -143,15 +146,15 @@ final class Product {
 				$note = ob_get_clean();
 			}
 
-			$order->add_order_note( $note );
+			$order?->add_order_note( $note );
 		} else {
-			$order->add_order_note( $responses_string );
+			$order?->add_order_note( $responses_string );
 		}
 
-		$order->update_meta_data( self::CREATE_SITE_RESPONSES_META_KEY, \wp_json_encode( $responses ) );
-		$order->update_meta_data( self::LINKED_SITE_IDS_META_KEY, $linked_site_ids );
+		$order?->update_meta_data( self::CREATE_SITE_RESPONSES_META_KEY, \wp_json_encode( $responses ) );
+		$order?->update_meta_data( self::LINKED_SITE_IDS_META_KEY, $linked_site_ids );
 
-		$order->save();
+		$order?->save();
 	}
 }
 
