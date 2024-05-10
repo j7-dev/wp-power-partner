a1ac94f7e320b13bf8aa68048ca688a3e235cf51
J7 <j7.dev.gg@outlook.com>
2024-05-10 09:39:38 +0800

    fix: 循環引用錯誤

--- inc/class/utils/class-base.php
+++ inc/class/utils/class-base.php
@@ -16,7 +16,9 @@ use J7\PowerPartner\Plugin;
 abstract class Base {
 	const BASE_URL      = '/';
 	const APP1_SELECTOR = '.redux-group-tab.power-partner-connect-app';
-	const APP2_SELECTOR = '.' . Shortcode::CURRENT_USER_SITE_LIST_SHORTCODE;
+	// const APP2_SELECTOR = '.' . Shortcode::CURRENT_USER_SITE_LIST_SHORTCODE;
+	const APP2_SELECTOR = '.power_partner_current_user_site_list';
+
 	const API_TIMEOUT   = '30000';
 	const DEFAULT_IMAGE = 'http://1.gravatar.com/avatar/1c39955b5fe5ae1bf51a77642f052848?s=96&d=mm&r=g';
 
