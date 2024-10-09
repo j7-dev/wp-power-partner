b8a4659059c9cd89010cb563d4efde64302d7a94
J7 <j7.dev.gg@gmail.com>
2024-10-09 18:21:45 +0800

    fix: 必須依賴 2.0.2 Powerhouse

--- plugin.php
+++ plugin.php
@@ -68,7 +68,7 @@ final class Plugin {
 				'name'     => 'Powerhouse',
 				'slug'     => 'powerhouse',
 				'source'   => 'https://github.com/j7-dev/wp-powerhouse/releases/latest/download/powerhouse.zip',
-				'version'  => '2.0.1',
+				'version'  => '2.0.2',
 				'required' => true,
 			],
 			[
