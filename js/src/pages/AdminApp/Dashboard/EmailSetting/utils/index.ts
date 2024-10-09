46a4dae259ad351b9de61bc54e5e3fd597579730
J7 <j7.dev.gg@gmail.com>
2024-10-09 18:08:53 +0800

    feat: 更新 EMAIL 模板

--- js/src/pages/AdminApp/Dashboard/EmailSetting/utils/index.ts
+++ js/src/pages/AdminApp/Dashboard/EmailSetting/utils/index.ts
@@ -10,7 +10,32 @@ export const getEmailTemplate = () =>
 		key: nanoid(),
 		enabled: false,
 		subject: '這裡填你的信件主旨 ##FIRST_NAME##',
-		body: '<p>嗨 ##FIRST_NAME##</p><p>你的網站開好囉，<a href="https://cloud.luke.cafe/docs" rel="noopener noreferrer" target="_blank">點此可以打開網站的使用說明書</a></p><p><br></p><p>另外如果要將網站換成正式的網域，請參考<a href="https://cloud.luke.cafe/docs/domain-change/" rel="noopener noreferrer" target="_blank">這篇教學</a></p><p><br></p><p>有網站的問題都可以直接回覆這封信，或是私訊 <a href="https://wpsite.pro/" rel="noopener noreferrer" target="_blank">架站小幫手網站</a> 的右下角對話框</p><p>&nbsp;</p><p>--- 以下是你的網站資訊 ---</p><p><br></p><p>網站暫時網址：</p><p>##FRONTURL##</p><p>之後可換成你自己的網址</p><p><br></p><p>網站後台：</p><p>##ADMINURL##</p><p><br></p><p>帳號：</p><p>##SITEUSERNAME##</p><p><br></p><p>密碼：</p><p>##SITEPASSWORD##</p><p><br></p><p><strong>進去後請記得改成自己的密碼喔</strong></p><p><br></p><p>網站列表 + 進階設置：</p><p>##WORDPRESSAPPWCSITESACCOUNTPAGE##</p><br><p>網站主機ip：</p><p>##IPV4##</p><p>&nbsp;</p><p>這封信很重要，不要刪掉，這樣之後才找得到喔～</p><p>&nbsp;</p><p><br></p>',
+		body: `
+		<p>嗨 ##FIRST_NAME##</p>
+	<p>你的網站開好囉，<a href="https://docs.wpsite.pro/wp" target="_blank">點此可以打開網站的使用說明書</a></p>
+	<p><br></p>
+	<p>另外如果要將網站換成正式的網域，請<a href="https://docs.wpsite.pro/wp/wordpress-kuai-su-ru-men/jiang-zan-shi-wang-yu-huan-cheng-zheng-shi-wang-yu-rang-wang-zhan-zheng-shi-shang-xian" target="_blank">參考這篇教學</a></p>
+	<p><br></p>
+	<p>有網站的問題都可以或是私訊<a href="https://wpsite.pro" target="_blank">架站小幫手網站</a> 的右下角對話框</p>
+	<p>&nbsp;</p>
+	<p>--- 以下是你的網站資訊 ---</p>
+	<p><br></p
+	<p>網站暫時網址：##FRONTURL##</p>
+	<p>之後可換成你自己的網址</p>
+	<p><br></p>
+	<p>網站後台：##ADMINURL##</p>
+	<p><br></p>
+	<p>帳號：##SITEUSERNAME##</p>
+	<p><br></p>
+	<p>密碼：##SITEPASSWORD##</p>
+	<p><br></p>
+	<p><strong>進去後請記得改成自己的密碼喔</strong></p>
+	<p><br></p>
+	<br>
+	<p>網站主機ip：##IPV4##</p>
+	<p>&nbsp;</p>
+	<p>這封信很重要，不要刪掉，這樣之後才找得到喔</p><p>&nbsp;</p><p><br></p>
+		`,
 		action_name: 'subscription_failed',
 		days: 0,
 		operator: 'after',
