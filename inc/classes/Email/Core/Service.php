2d1189601546022a0791f3e3316464c753c170cc
J7 <j7.dev.gg@gmail.com>
2025-05-21 14:24:25 +0800

    WIP: 依照訂閱狀態發送 email

--- inc/classes/Email/Core/Service.php
+++ inc/classes/Email/Core/Service.php
@@ -35,16 +35,22 @@ final class Service {
 		$power_partner_settings = \get_option( 'power_partner_settings', [] );
 		$emails_array           = is_array( $power_partner_settings['emails'] ) ? $power_partner_settings['emails'] : [];
 
-		$strict = \wp_get_environment_type() === 'local';
-
-		$this->emails = array_map( fn ( $email ) => new Email( $email, $strict ), $emails_array );
+		$strict       = \wp_get_environment_type() === 'local';
+		$this->emails = Email::parse_array( $emails_array, $strict );
 
 		$this->default = (object) [
 			'subject' => '這裡填你的信件主旨 ##FIRST_NAME##',
 			'body'    => Plugin::DEFAULT_EMAIL_BODY,
 		];
 
-		$this->action_names = (object) [
+		$this->action_names = (object) self::get_action_names();
+
+		\add_action( 'woocommerce_subscription_pre_update_status', [ $this, 'subscription_status_changed' ], 10, 3 );
+	}
+
+	/** @return array<string,string> Action names */
+	public static function get_action_names(): array {
+		return [
 			'site_sync'               => 'site_sync', // 下單開站後
 			'subscription_failed'     => 'subscription_failed', // 續訂失敗後
 			'subscription_success'    => 'subscription_success', // 續訂成功後
@@ -52,8 +58,8 @@ final class Service {
 			'date_created'            => 'date_created', // 訂閱成立後
 			'trial_end'               => 'trial_end', // 試用結束前|後
 			'next_payment'            => 'next_payment', // 下次付款前|後
-			'end'                     => 'end',
-			'end_of_prepaid_term'     => 'end_of_prepaid_term',
+			'end'                     => 'end', // TODO: 可能沒用到?
+			'end_of_prepaid_term'     => 'end_of_prepaid_term', // TODO: 可能沒用到?
 		];
 	}
 
@@ -86,5 +92,28 @@ final class Service {
 		}
 
 		return $enabled_emails;
+	}
+
+
+	/**
+	 * Subscription failed
+	 * 如果用戶續訂失敗，則停用訂單網站
+	 *
+	 * @param string           $old_status old status
+	 * @param string           $new_status new status
+	 * @param \WC_Subscription $subscription post
+	 * @return void
+	 */
+	public function subscription_status_changed( $old_status, $new_status, $subscription ): void {
+		// WIP: 依照訂閱狀態發送 email
+		// TEST 印出 WC Logger 記得移除 ---- //
+		\J7\WpUtils\Classes\WC::log(
+			[
+				'old_status' => $old_status,
+				'new_status' => $new_status,
+			],
+			'subscription_status_changed'
+			);
+		// ---------- END TEST ---------- //
 	}
 }
