2d2a6d542be283cae9673eb9d0e7499e8ffce958
J7 <j7.dev.gg@gmail.com>
2026-02-05 12:52:04 +0800

    fix: 簡易訂閱不會在 WPCD 開站的問題

--- inc/classes/Product/SiteSync.php
+++ inc/classes/Product/SiteSync.php
@@ -11,6 +11,7 @@ use J7\PowerPartner\Product\DataTabs\LinkedSites;
 use J7\Powerhouse\Domains\Subscription\Shared\Enums\Action;
 use J7\PowerPartner\Domains\Email\Core\SubscriptionEmailHooks as EmailService;
 use J7\PowerPartner\Utils\Token;
+use WC_Subscriptions;
 
 /** Class SiteSync */
 final class SiteSync {
@@ -115,55 +116,13 @@ final class SiteSync {
 				];
 
 				// 根據 host_type 選擇對應的 API
-				// wpcd 為舊架構
-				if ( $host_type === LinkedSites::WPCD_HOST_TYPE ) {
-					// 舊架構：使用 Fetch::site_sync
-					$response_obj = Fetch::site_sync( $site_sync_params );
-				}
-
 				// powercloud 為新架構（新架構是默認Host Type)
 				if ( $host_type === LinkedSites::DEFAULT_HOST_TYPE ) {
-
-					$open_site_plan_id = \get_post_meta( $product_id, LinkedSites::OPEN_SITE_PLAN_FIELD_NAME, true );
-					$template_site_id  = \get_post_meta( $product_id, LinkedSites::LINKED_SITE_FIELD_NAME, true );
-
-					// 新架構：使用 FetchPowerCloud::site_sync
-					[$response_obj, $wordpress_obj] = FetchPowerCloud::site_sync( $site_sync_params, $open_site_plan_id, $template_site_id );
-
-					// 發送 email 給用戶，告知網站已建立成功
-					if ( $response_obj?->status === 201 ) {
-						$order_token = Token::get_order_tokens( $parent_order );
-
-						// 拿到 email payloads
-						$email_payloads = \array_merge(
-							$order_token,
-							[
-								'CUSTOMER_ID'  => $parent_order->get_customer_id(),
-								'REF_ORDER_ID' => $parent_order_id,
-								'WORDPRESSAPPWCSITESACCOUNTPAGE' => '',
-								'IPV4'         => '163.61.60.30',
-								'DOMAIN'       => 'https://' . $wordpress_obj->domain,
-								'FRONTURL'     => 'https://' . $wordpress_obj->domain,
-								'ADMINURL'     => 'https://' . $wordpress_obj->domain . '/wp-admin',
-								'SITEUSERNAME' => $wordpress_obj->wp_admin_email,
-								'SITEPASSWORD' => $wordpress_obj->wp_admin_password,
-								'NEW_SITE_ID'  => '',
-							]
-							);
-
-						$subscription->update_meta_data( 'email_payloads_tmp', $email_payloads );
-						$subscription->save();
-
-						\as_schedule_single_action(
-							\time() + 240,
-							'powerhouse_delay_send_email',
-							[
-								'to'              => $wordpress_obj->wp_admin_email,
-								'subscription_id' => $subscription->get_id(),
-							]
-							);
-
-					}
+					$response_obj = self::site_sync_powercloud( (int) $product_id, $subscription, $site_sync_params );
+				} else {
+					// wpcd 為舊架構
+					// 舊架構：使用 Fetch::site_sync
+					$response_obj = Fetch::site_sync( $site_sync_params );
 				}
 
 				$responses[] = [
@@ -275,5 +234,64 @@ final class SiteSync {
 
 		$subscription->delete_meta_data( 'email_payloads_tmp' );
 		$subscription->save();
+	}
+
+	/**
+	 * PowerCloud 開站
+	 *
+	 * @param int             $product_id 商品 ID
+	 * @param WC_Subscription $subscription 訂閱物件
+	 * @param array           $site_sync_params 開站參數
+	 * @return object 回傳 API 回應物件
+	 *
+	 * @throws \InvalidArgumentException 當訂閱的父訂單無效時拋出異常
+	 */
+	private static function site_sync_powercloud( int $product_id, \WC_Subscription $subscription, array $site_sync_params ) {
+		$parent_order = $subscription->get_parent();
+		if (! $parent_order instanceof \WC_Order ) {
+			throw new \InvalidArgumentException( 'Invalid parent order in subscription.' );
+		}
+		$open_site_plan_id = \get_post_meta( $product_id, LinkedSites::OPEN_SITE_PLAN_FIELD_NAME, true );
+		$template_site_id  = \get_post_meta( $product_id, LinkedSites::LINKED_SITE_FIELD_NAME, true );
+
+		// 新架構：使用 FetchPowerCloud::site_sync
+		[$response_obj, $wordpress_obj] = FetchPowerCloud::site_sync( $site_sync_params, $open_site_plan_id, $template_site_id );
+
+		// 發送 email 給用戶，告知網站已建立成功
+		if ( $response_obj?->status === 201 ) {
+			$order_token = Token::get_order_tokens( $parent_order );
+
+			// 拿到 email payloads
+			$email_payloads = \array_merge(
+				$order_token,
+				[
+					'CUSTOMER_ID'                    => $parent_order->get_customer_id(),
+					'REF_ORDER_ID'                   => $parent_order->get_id(),
+					'WORDPRESSAPPWCSITESACCOUNTPAGE' => '',
+					'IPV4'                           => '163.61.60.30',
+					'DOMAIN'                         => 'https://' . $wordpress_obj->domain,
+					'FRONTURL'                       => 'https://' . $wordpress_obj->domain,
+					'ADMINURL'                       => 'https://' . $wordpress_obj->domain . '/wp-admin',
+					'SITEUSERNAME'                   => $wordpress_obj->wp_admin_email,
+					'SITEPASSWORD'                   => $wordpress_obj->wp_admin_password,
+					'NEW_SITE_ID'                    => '',
+				]
+				);
+
+			$subscription->update_meta_data( 'email_payloads_tmp', $email_payloads );
+			$subscription->save();
+
+			\as_schedule_single_action(
+				\time() + 240,
+				'powerhouse_delay_send_email',
+				[
+					'to'              => $wordpress_obj->wp_admin_email,
+					'subscription_id' => $subscription->get_id(),
+				]
+				);
+
+		}
+
+		return $response_obj;
 	}
 }
